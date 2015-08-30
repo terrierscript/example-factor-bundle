5b1a69db23f4ef06cc34534680e54d46f7f77bba
inuscript <inuscript@gmail.com>
2015-08-30 17:37:52 +0900

    semi

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
 var path = require("path")
-var browserify = require('browserify');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
+var browserify = require('browserify')
+var babelify = require('babelify')
+var source = require('vinyl-source-stream')
 var glob = require("glob")
 var del = require("del")
 var mkdirp = require("mkdirp")
-var factor = require('factor-bundle');
+var factor = require('factor-bundle')
 
 var dest = './output/'
 
@@ -35,10 +35,10 @@ gulp.task('browserify', function() {
   .bundle()
   .pipe(source("common.js"))
   .pipe(gulp.dest(path.join(dest, "client")))
-});
+})
 
 gulp.task('watch', function() {
   gulp.watch('src/**/*', ['browserify'])
-});
+})
 
-gulp.task('default', ['browserify', 'watch']);
+gulp.task('default', ['browserify', 'watch'])
